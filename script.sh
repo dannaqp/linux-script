220e7965e916feab2b097cee555bd6a730eca35d
dannaqp <dasimaluisaqu@uide.edu.ec>
2025-12-04 12:56:28 +0000

    creando scripts con el profe Titooo

---
+++ script.sh
@@ -0,0 +1,6 @@
+# Crear y listar archivos
+echo 'Hola Linux' > archivo.txt # echo sirve para imprimir algo en la terminal, el '>' redirecciona la salida ahora ya no va a la terminal y lo manda archivo.txt el cual se crea en este momento
+cat archivo.txt # cat lee y muestra en la terminal el contenido de archivos 
+cp archivo.txt copia.txt # cp la abreviatura de copia funciona como cp [origen][destino]
+ls -l # ls sirve para listar algo y -l es una opción (flag) que significa "long format" es decir el formato largo 
+rm archivo.txt # rm significa remove sirve para eliminar funciona como rm [archivo a eliminar]

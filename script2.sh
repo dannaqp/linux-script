91a6b1cd23be39b69210871ac2890f15200278ec
dannaqp <dasimaluisaqu@uide.edu.ec>
2025-12-04 13:19:20 +0000

    segundo script

---
+++ script2.sh
@@ -0,0 +1,4 @@
+uname -a # uname viene de 'unix name' y muestra la informacion basica del sistema -a significa all 
+lscpu # significa 'list cpu' muestra la info de la cpu
+free -h # free muestra el estado de la ram y del swap y -h hace que sea legible para humanos 
+df -h # df significa 'disk free' y muestra la info de los discos duros o particiones y lo hace legible para humanos con -h
